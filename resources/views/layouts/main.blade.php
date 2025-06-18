9648fe6e2e637dea5744127388a1df15511757f9
CONTOH-DEV <062021107454@mhs.itats.ac.id>
2025-06-18 12:26:19 +0700

    update dashboard view

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>@yield('title', 'Argon Dashboard 2 Tailwind by Creative Tim')</title>
+  <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/img/apple-icon.png') }}" />
+  <link rel="icon" type="image/png" href="{{ asset('assets/img/favicon.png') }}" />
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <link href="{{ asset('assets/css/nucleo-icons.css') }}" rel="stylesheet" />
+  <link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+  <script src="https://unpkg.com/@popperjs/core@2"></script>
+  <link href="{{ asset('assets/css/argon-dashboard-tailwind.css?v=1.0.1') }}" rel="stylesheet" />
+</head>
+
+<body class="m-0 font-sans text-base antialiased font-normal dark:bg-slate-900 leading-default bg-gray-50 text-slate-500">
+  <div class="absolute w-full bg-blue-500 dark:hidden min-h-75"></div>
+
+  {{-- Sidebar --}}
+  @include('layouts.partials.sidebar')
+
+  {{-- Main Content --}}
+  <main class="relative h-full max-h-screen transition-all duration-200 ease-in-out xl:ml-68 rounded-xl">
+    {{-- Navbar --}}
+    @include('layouts.partials.navbar')
+
+    {{-- Page Content --}}
+    <div class="w-full px-6 py-6 mx-auto">
+      @yield('content')
+    </div>
+  </main>
+
+  <script src="{{ asset('assets/js/plugins/chartjs.min.js') }}"></script>
+  <script src="{{ asset('assets/js/plugins/perfect-scrollbar.min.js') }}"></script>
+  <script src="{{ asset('assets/js/argon-dashboard-tailwind.js?v=1.0.1') }}"></script>
+</body>
+</html>
